b45f2b3ed7582fff0b6db1e77efbdb0d8560a60d
bulihuligan <bifkovits.dora@gmail.com>
2021-10-20 13:47:37 +0200

    ma

---
+++ week02/day3/src/HelloMe/HelloMe.java
@@ -0,0 +1,11 @@
+package HelloMe;
+
+import org.w3c.dom.ranges.DocumentRange;
+
+public class HelloMe {
+    public static void main(String[] args) {
+        String name = "Dora";
+        System.out.print("HelloMe");
+
+    }
+}
